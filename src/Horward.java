29497add345d604f3d6bddecdd12e09b79f8ae4f
igr1 <dfrgth>
2022-09-15 17:55:36 +1000

    lesson 2.2

---
+++ src/Horward.java
@@ -0,0 +1,23 @@
+public class Horward {
+    String name;
+    int power;
+    int distance;
+
+    public Horward(String name,int power, int distance) {
+        this.name = name;
+        this.power = power;
+        this.distance = distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
